508b9ff269a3c9969ba90a99a6229825a8347235
Arpit Jana <arpitjana2103@gmail.com>
2024-12-27 22:22:28 +0530

    Item list css done

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,5 +1,22 @@
-function Item() {
-    return <div>Item</div>;
+import Emoji from "./Emoji";
+import styles from "../styles/Item.module.css";
+
+function Item(prop) {
+    const { count, name, packed } = prop;
+
+    return (
+        <div className={styles.item}>
+            <input type="checkbox" id={name} checked={packed} />
+            <label htmlFor={name}>
+                <span>
+                    {count} {name}
+                </span>
+            </label>
+            <button>
+                <Emoji txt="❌" />
+            </button>
+        </div>
+    );
 }
 
 export default Item;
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,7 +1,34 @@
 import styles from "../styles/ItemList.module.css";
+import Item from "./Item";
+
+const items = [
+    { name: "Charger", count: 1, packed: true },
+    { name: "Socks", count: 2, packed: false },
+    { name: "Shirts", count: 4, packed: false },
+    { name: "Jeans", count: 2, packed: true },
+    { name: "PowerBank", count: 1, packed: false },
+    { name: "Charger", count: 1, packed: true },
+    { name: "Socks", count: 2, packed: false },
+    { name: "Shirts", count: 4, packed: false },
+    { name: "Jeans", count: 2, packed: true },
+    { name: "PowerBank", count: 1, packed: false },
+];
 
 function ItemList() {
-    return <div className={styles.itemList}>ItemList</div>;
+    return (
+        <div className={styles.itemList}>
+            {items.map(function (item, index) {
+                return (
+                    <Item
+                        name={item.name}
+                        count={item.count}
+                        packed={item.packed}
+                        key={index}
+                    />
+                );
+            })}
+        </div>
+    );
 }
 
 export default ItemList;
